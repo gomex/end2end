65b7c8a178228b8aba063799f5ced2b4cd8a9aab
Rafael Gomes <gomex@riseup.net>
2017-06-01 02:14:01 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
-
+gem install pygments.rb
 bundle exec jekyll build
 # bundle exec travis-lint
 # bundle exec htmlproof ${HTML_FOLDER} --disable-external
